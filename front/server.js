0e05220b5eceb233ab8bc949cbdc96d86313b68b
Pjotr Harlamov <pjotr.harlamov@ivkhk.ee>
2026-03-02 13:33:52 +0200

    server.js file edited

--- front/server.js
+++ front/server.js
@@ -12,5 +12,5 @@ app.get('/health', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-	console.log('Server tootab pordi ${PORT} peale');
+	console.log(`Server tootab pordi ${PORT} peale`);
 });
